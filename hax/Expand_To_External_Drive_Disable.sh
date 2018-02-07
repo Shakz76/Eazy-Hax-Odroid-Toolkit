7f0bd3bcb793f8bad13ede1fc5cc7c6a8375cf43
Forrest <forrest.page@gmail.com>
2018-02-07 14:43:07 -0600

    Disable script upload after initial changes.

---
+++ hax/Expand_To_External_Drive_Disable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ ! -d /home/pigaming/RetroPie/localroms ]; then
+    echo ""
+    echo ""
+    echo "I do not detect that this Retropie is expanded. Killing this script."
+    echo ""
+    echo ""
+	sleep 5
+else
+
+sudo sed -i '/addon/d' /etc/fstab
+sudo cp /etc/profile.d/10-retropie.sh.org /etc/profile.d/10-retropie.sh
+sudo cp /etc/samba/smb.conf.bkup /etc/samba/smb.conf
+unlink /home/pigaming/RetroPie/roms; sudo umount /home/pigaming/addonusb; sudo umount overlay; sudo umount /home/pigaming/RetroPie/roms;rm -r /home/pigaming/RetroPie/roms; mv /home/pigaming/RetroPie/localroms /home/pigaming/RetroPie/roms  > /dev/null 2>&1
+sudo reboot
+fi
